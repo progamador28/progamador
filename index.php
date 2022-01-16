06dd8edc5075773148837176c3c79e0fa3500dee
progamador28 <97845018+progamador28@users.noreply.github.com>
2022-01-16 14:10:23 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+// Conexão
+require_once  'db_connect.php';
+
+/// sessão
+session_start();
+
+// Botão enviar
+if(isset($_POST['btn-entrar'])):
+	$erros = array();
+	$login = mysqli_escape_string($connect, $_POST['login']);
+	$senha = mysqli_escape_string($connect, $_POST['senha']);
+
+	if(empty($login) or empty($senha)):
+		$erros[] = "<li> o campo login/senha precisa ser preenchido </li>";
+
+       else:
+       	   $sql = "SELECT login FROM usuarios WHERE login = '$login'";
+       	   $resultado = mysqli_query($connect, $sql);
+
+       	   if(mysqli_num_rows($resultado) > 0):
+       	   	   $senha = md5($senha);
+       	   	   $sql = "SELECT * FROM usuarios WHERE login = '$login' AND senha = '$senha'";
+       	   	   $resultado = mysqli_query($connect, $sql);
+
+       	   	       if(mysqli_num_rows($resultado) == 1):
+       	   	       	   $dados = mysqli_fetch_array($resultado);
+       	   	       	   mysqli_close($connect);
+       	   	       	   $_SESSION['logado'] = true;
+       	   	       	   $_SESSION['id_usuario'] = $dados['id'];
+       	   	       	   header('location: home.php');
+       	   	       else:
+       	   	       	   $erros[] = "<li> Usuário e senha não conferem </li>";
+       	   	       endif;
+
+
+
+       	   else: 
+       	     	$erros[] = "<li> Usuário inexistente </li>";
+       	   endif;
+
+       endif;
+
+endif;	
+?>
+
+<html>
+<head>
+	<title>login</title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+<h1> Login </h1>
+<?php
+if(!empty($erros)):
+	foreach($erros as $erro):
+	    echo $erro;	
+	endforeach;
+endif;
+?>
+<hr>
+
+
+
+	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST"> 
+	Login: <input type="text" name="login"><br>
+	Senha: <input type="password" name="senha"><br>
+	<button type="submit" name="btn-entrar"> Entrar </button>
+	</form>
+</body>
+</html>
